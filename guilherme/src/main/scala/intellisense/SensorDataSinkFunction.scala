cb202acd966084c76a24b741383c1dac5c316516
Guilherme Nobre <guilhermenobre2@gmail.com>
2017-08-09 16:45:22 +0200

    sensor data sink now outputs predictions

--- guilherme/src/main/scala/intellisense/SensorDataSinkFunction.scala
+++ guilherme/src/main/scala/intellisense/SensorDataSinkFunction.scala
@@ -1,21 +1,24 @@
 package intellisense
 
-import intellisense.SensorStream.SensorData
+import intellisense.SensorStream.Prediction
 import org.apache.flink.api.common.functions.RuntimeContext
 import org.apache.flink.streaming.connectors.elasticsearch.{ElasticsearchSinkFunction, RequestIndexer}
 import org.elasticsearch.action.index.IndexRequest
 import org.elasticsearch.client.Requests
 
-class SensorDataSinkFunction extends ElasticsearchSinkFunction[SensorData] {
+class SensorDataSinkFunction extends ElasticsearchSinkFunction[Prediction] {
 
-  def process(record: SensorData, ctx: RuntimeContext, indexer: RequestIndexer) {
+  def process(record: Prediction, ctx: RuntimeContext, indexer: RequestIndexer) {
 
     val json = new java.util.HashMap[String, String]
-    json.put("data", record.data.toString)
+    json.put("timestamp", record.timestamp)
+    json.put("actual", record.actual.toString)
+    json.put("predicted", record.predicted.toString)
+    json.put("anomaly_score", record.anomalyScore.toString)
 
     val request: IndexRequest = Requests.indexRequest
-      .index("flink")
-      .`type`("flink")
+      .index("sensor_data")
+      .`type`("record")
       .source(json)
 
     indexer.add(request)
