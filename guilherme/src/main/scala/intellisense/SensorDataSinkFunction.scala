ed9cae0e6a3372a9276f7f84d36fb6890cc33902
Guilherme Nobre <guilhermenobre2@gmail.com>
2017-08-09 16:16:43 +0200

    add Elasticsearch custom sink function for SensorData

---
+++ guilherme/src/main/scala/intellisense/SensorDataSinkFunction.scala
@@ -0,0 +1,23 @@
+package intellisense
+
+import intellisense.SenseStream.SensorData
+import org.apache.flink.api.common.functions.RuntimeContext
+import org.apache.flink.streaming.connectors.elasticsearch.{ElasticsearchSinkFunction, RequestIndexer}
+import org.elasticsearch.action.index.IndexRequest
+import org.elasticsearch.client.Requests
+
+class SensorDataSinkFunction extends ElasticsearchSinkFunction[SensorData] {
+
+  def process(record: SensorData, ctx: RuntimeContext, indexer: RequestIndexer) {
+
+    val json = new java.util.HashMap[String, String]
+    json.put("data", record.data.toString)
+
+    val request: IndexRequest = Requests.indexRequest
+      .index("flink")
+      .`type`("flink")
+      .source(json)
+
+    indexer.add(request)
+  }
+}
