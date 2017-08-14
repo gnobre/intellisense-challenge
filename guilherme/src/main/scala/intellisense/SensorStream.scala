8f555beb25a1c96a74bd6632133ab7bf0ee7194c
Guilherme Nobre <guilhermenobre2@gmail.com>
2017-08-14 14:54:06 +0200

    create one HTM network/prediction for each field

--- guilherme/src/main/scala/intellisense/SensorStream.scala
+++ guilherme/src/main/scala/intellisense/SensorStream.scala
@@ -7,11 +7,26 @@ import org.apache.flink.streaming.connectors.elasticsearch5.ElasticsearchSink
 import de.javakaffee.kryoserializers.jodatime.JodaDateTimeSerializer
 import org.apache.flink.streaming.api.scala._
 import org.joda.time.DateTime
-import org.numenta.nupic.encoders.DateEncoder._
+import org.numenta.nupic.Parameters.KEY
+import org.numenta.nupic.algorithms.{Anomaly, SpatialPooler, TemporalMemory}
+import org.numenta.nupic.encoders.scala.{DateEncoder, MultiEncoder, ScalarEncoder}
 import org.numenta.nupic.flink.streaming.api.scala._
-
+import org.numenta.nupic.flink.streaming.examples.common.NetworkDemoParameters
+import org.numenta.nupic.network.Network
 
 object SensorStream extends SensorModel {
+
+  def parseValue(v: String): Double = {
+    if (v == "") {
+      0.0
+    } else {
+        try {
+          v.toDouble
+        } catch {
+          case _: Throwable => 0.0
+        }
+    }
+  }
 
   def main(args: Array[String]) {
     val path = args(0)
@@ -23,40 +38,45 @@ object SensorStream extends SensorModel {
     }
 
     // Reading input from CSV as data stream
-    val input: DataStream[SensorData] = env.readTextFile(path).map {
-      _.split(",") match {
-        case Array(timestamp, data) => SensorData(LOOSE_DATE_TIME.parseDateTime(timestamp), data.toDouble)
-      }
-    }
-
-    // Anomaly detection
-    val inferences: DataStream[Prediction] = input
-      .learn(network)
-      .select { inference => inference }
-      .keyBy { _ => None }
-      .mapWithState { (inference, state: Option[Double]) =>
-
-        val prediction = Prediction(
-          inference._1.timestamp.toString(LOOSE_DATE_TIME),
-          inference._1.data,
-          state match {
-            case Some(prediction) => prediction
-            case None => 0.0
-          },
-          inference._2.getAnomalyScore)
-
-        // store the prediction about the next value as state for the next iteration,
-        // so that actual vs predicted is a meaningful comparison
-        val predictedData = inference._2.getClassification("data").getMostProbableValue(1).asInstanceOf[Any] match {
-          case value: Double if value != 0.0 => value
-          case _ => state.getOrElse(0.0)
+    var header = true
+    val input: DataStream[SensorData] = env.readTextFile(path).map(
+      line => {
+        var values = line.split(',')
+        while(values.length < classOf[SensorData].getDeclaredFields.length - 1) {
+          values = values :+ ""
         }
-
-        (prediction, Some(predictedData))
+        values
+      })
+      .filter {
+        values => {
+          val isValue = !header
+          header = false
+          isValue
+        }
+      }
+      .map { values => {SensorData(values(0).toInt,
+                                   DateTime.parse(values(1)),
+                                   parseValue(values(2)),
+                                   parseValue(values(3)),
+                                   parseValue(values(4)),
+                                   parseValue(values(5)),
+                                   parseValue(values(6)),
+                                   parseValue(values(7)),
+                                   parseValue(values(8)),
+                                   parseValue(values(9)),
+                                   parseValue(values(10)),
+                                   parseValue(values(11)),
+                                   parseValue(values(12)),
+                                   parseValue(values(13)),
+                                   parseValue(values(14)),
+                                   parseValue(values(15)),
+                                   parseValue(values(16)),
+                                   parseValue(values(17)))
+      }
       }
 
 
-    // Output records into elasticsearch sink
+    // Configuring elasticsearch sink
     val config = new java.util.HashMap[String, String]
     config.put("cluster.name", "intellisense")
     config.put("bulk.flush.max.actions", "1")
@@ -64,7 +84,55 @@ object SensorStream extends SensorModel {
     val transportAddresses = new java.util.ArrayList[InetSocketAddress]
     transportAddresses.add(new InetSocketAddress(InetAddress.getByName("127.0.0.1"), 9300))
 
-    inferences.addSink(new ElasticsearchSink(config, transportAddresses, new SensorDataSinkFunction))
+
+    // Anomaly detection
+    for (field <- fields) {
+      val predictions: DataStream[Prediction] = input
+        .learn((key: AnyRef) => {
+          val encoder = MultiEncoder(
+            DateEncoder().name("timestamp").timeOfDay(21, 9.5),
+            ScalarEncoder().name(field).n(50).w(21).maxVal(Double.MaxValue).resolution(0.1).clipInput(true)
+          )
+
+          val params = NetworkDemoParameters.params
+
+          val network = Network.create("Intellisense Network - " + field, params)
+            .add(Network.createRegion("Region 1")
+              .add(Network.createLayer("Layer 2/3", params)
+                .alterParameter(KEY.AUTO_CLASSIFY, true)
+                .add(encoder)
+                .add(Anomaly.create())
+                .add(new TemporalMemory())
+                .add(new SpatialPooler())))
+
+          network.setEncoder(encoder)
+          network
+        })
+        .select { inference => inference }
+        .keyBy { _ => None }
+        .mapWithState { (inference, state: Option[Double]) =>
+
+          val prediction = Prediction(
+            inference._1.timestamp.toString,
+            field,
+            inference._1.getClass.getMethods.find(_.getName == field).get.invoke(inference._1),
+            state match {
+              case Some(predictedValue) => predictedValue
+              case None => 0.0
+            },
+            inference._2.getAnomalyScore)
+
+          // store the prediction about the next value as state for the next iteration,
+          // so that actual vs predicted is a meaningful comparison
+          val predictedData = inference._2.getClassification(field).getMostProbableValue(1).asInstanceOf[Any] match {
+            case value: Double if value != 0.0 => value
+            case _ => state.getOrElse(0.0)
+          }
+
+          (prediction, Some(predictedData))
+        }
+      predictions.addSink(new ElasticsearchSink(config, transportAddresses, new SensorDataSinkFunction))
+    }
 
     env.execute("Guilherme @ Intellisense")
   }
