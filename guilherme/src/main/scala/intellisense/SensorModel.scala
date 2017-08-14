4c7291c156b4172f73ce86e19b02239632b8671c
Guilherme Nobre <guilhermenobre2@gmail.com>
2017-08-14 14:53:05 +0200

    add fields for sensor date based on csv

--- guilherme/src/main/scala/intellisense/SensorModel.scala
+++ guilherme/src/main/scala/intellisense/SensorModel.scala
@@ -8,28 +8,35 @@ import org.numenta.nupic.flink.streaming.examples.common.NetworkDemoParameters
 import org.numenta.nupic.network.Network
 
 trait SensorModel {
-  case class SensorData(timestamp: DateTime, data: Double)
+  val fields: List[String] = List("bed_height", "bed_pressure", "clear_water_height",
+  "feed_solids_mass_flow", "feed_valve_A", "feed_valve_B", "flocculant_dilution_flow_rate",
+  "flocculant_flow_rate", "interface_height", "mud_height", "rake_height", "rake_torque",
+  "underflow_flow_rate", "underflow_line_1_pump_running", "underflow_line_2_pump_running",
+  "underflow_percent_solids")
 
-  case class Prediction(timestamp: String, actual: Double, predicted: Double, anomalyScore: Double)
+  case class SensorData(index: Int,
+                        timestamp: DateTime,
+                        bed_height: Double,
+                        bed_pressure: Double,
+                        clear_water_height: Double,
+                        feed_solids_mass_flow: Double,
+                        feed_valve_A: Double,
+                        feed_valve_B: Double,
+                        flocculant_dilution_flow_rate: Double,
+                        flocculant_flow_rate: Double,
+                        interface_height: Double,
+                        mud_height: Double,
+                        rake_height: Double,
+                        rake_torque: Double,
+                        underflow_flow_rate: Double,
+                        underflow_line_1_pump_running: Double,
+                        underflow_line_2_pump_running: Double,
+                        underflow_percent_solids: Double
+                       )
 
-  val network = (key: AnyRef) => {
-    val encoder = MultiEncoder(
-      DateEncoder().name("timestamp").timeOfDay(21, 9.5),
-      ScalarEncoder().name("data").n(50).w(21).maxVal(100.0).resolution(0.1).clipInput(true)
-    )
-
-    val params = NetworkDemoParameters.params
-
-    val network = Network.create("Intellisense Network", params)
-      .add(Network.createRegion("Region 1")
-        .add(Network.createLayer("Layer 2/3", params)
-          .alterParameter(KEY.AUTO_CLASSIFY, true)
-          .add(encoder)
-          .add(Anomaly.create())
-          .add(new TemporalMemory())
-          .add(new SpatialPooler())))
-
-    network.setEncoder(encoder)
-    network
-  }
+  case class Prediction(timestamp: String,
+                        type_es: String,
+                        actual: Any,
+                        predicted: Double,
+                        anomalyScore: Double)
 }
